87d8964f92afb54e68137a7123796f53b5972651
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-08-31 03:10:06 +0200

    Added Validation Logic

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ function validateSyntax() {
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    //validation logic
+    if (input.startsWith("pet_") && /^[a-zA-Z0-9]/.test(input.slice(4))){
+        result = "Valid Syntex \n Lets's start with the adoption!!🌞";
+    } else {
+        result = "Invalid Syntex;⚠️⚠️. \n Input should start with 'pet_' and followed by alphanumeric characters.";
+    }
 
             document.getElementById('result').innerText = result;
 }
